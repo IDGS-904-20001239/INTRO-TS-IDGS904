4b0c53179924f30d89b4983e4b970df40742b77c
yair <yair.teran47@gmail.com>
2023-06-09 20:31:53 -0600

    EYET 09/06/2023

---
+++ src/intro-ts/05-clases.ts
@@ -0,0 +1,44 @@
+
+class Persona2{
+    nombre:string='2';
+    edad:number=2;
+    //Constructor y entrar a los datos con this
+    constructor(nombre:string,edad:number){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+    imprimir(){
+        console.log(`Nombre: ${this.nombre} y Edad: ${this.edad}`)
+    }
+}
+
+let persona:Persona2;
+persona=new Persona2('Juan',23)
+persona.imprimir();
+
+
+//Modificadores de acceso
+class Dado{
+    private valor:number=0;
+    public tirar(){
+        this.generar();
+    }
+    private generar(){
+        this.valor=Math.floor(Math.random()*6)+1;
+    }
+    public imprimir(){
+        console.log(`Valor: ${this.valor}`);
+    }
+}
+let dado1=new Dado();
+dado1.tirar();
+dado1.imprimir();
+
+//Clase 2
+class Persona3{
+    constructor(public nombre:string, public edad:number){}
+    imprimir(){
+        console.log(`Nombre: ${this.nombre} y Edad: ${this.edad}`)
+    }
+}
+
